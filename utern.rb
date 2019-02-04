57ffcc8335475955b897afa7c8235f86360e9947
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-04 22:50:13 +0900

    Brew formula update for utern version v0.0.1

--- utern.rb
+++ utern.rb
@@ -3,7 +3,7 @@ class Utern < Formula
   homepage "https://github.com/knqyf263/utern"
   url "https://github.com/knqyf263/utern/releases/download/v0.0.1/utern_0.0.1_darwin_amd64.zip"
   version "0.0.1"
-  sha256 "97dc45efcdd5f3137a763684b15626d4d1b4bb2611a08e9c050ba828bd2992da"
+  sha256 "4bdf440af16b6de4836a970a90b5716b34f3c553c553c590466a8ec847da5498"
 
   def install
     bin.install Dir['utern']
